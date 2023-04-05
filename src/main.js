2575638872302c0602bb024ed95498932ae02ebe
Daniil Ryzhenko <ryzhenko62@gmail.com>
2023-04-06 00:23:16 +0500

    Шаблонизируй это

--- src/main.js
+++ src/main.js
@@ -2,12 +2,14 @@ import FilterView from './view/filter.js';
 import TripEventsPresenter from './presenter/trip-events-presenter.js';
 import MenuView from './view/menu.js';
 import { render } from './render.js';
+import RoutePointsModel from './model/route-point-model.js';
 
 const siteHeaderElement = document.querySelector('.trip-main');
 const siteMainElement = document.querySelector('.page-main');
-const tripPresenter = new TripEventsPresenter();
+const tripPresenter = new TripEventsPresenter(siteMainElement.querySelector('.trip-events'));
+const routePointModel = new RoutePointsModel();
 
 render(new FilterView(), siteHeaderElement.querySelector('.trip-controls__filters'));
 render(new MenuView(), siteHeaderElement.querySelector('.trip-controls__navigation'));
 
-tripPresenter.init(siteMainElement.querySelector('.trip-events'));
+tripPresenter.init(routePointModel);
