0ccf1e15a0c2e598b786b8c6049874f02e593767
Daniil Ryzhenko <ryzhenko62@gmail.com>
2023-05-21 02:44:36 +0500

    Большие перемены (часть 2)

--- src/view/sort.js
+++ src/view/sort.js
@@ -1,9 +1,10 @@
 import AbstractView from '../framework/view/abstract-view';
+import { SORT_TYPES } from '../utils.js';
 
 const createSortTemplate = () => (
   `<form class="trip-events__trip-sort  trip-sort" action="#" method="get">
   <div class="trip-sort__item  trip-sort__item--day">
-    <input id="sort-day" class="trip-sort__input  visually-hidden" type="radio" name="trip-sort" value="sort-day">
+    <input id="sort-day" class="trip-sort__input  visually-hidden" type="radio" name="trip-sort" value="sort-day" data-sort-type="${SORT_TYPES.DEFAULT}" checked>
     <label class="trip-sort__btn" for="sort-day">Day</label>
   </div>
   <div class="trip-sort__item  trip-sort__item--event">
@@ -11,11 +12,11 @@ const createSortTemplate = () => (
     <label class="trip-sort__btn" for="sort-event">Event</label>
   </div>
   <div class="trip-sort__item  trip-sort__item--time">
-    <input id="sort-time" class="trip-sort__input  visually-hidden" type="radio" name="trip-sort" value="sort-time">
+    <input id="sort-time" class="trip-sort__input  visually-hidden" type="radio" name="trip-sort" value="sort-time" data-sort-type="${SORT_TYPES.TIME}">
     <label class="trip-sort__btn" for="sort-time">Time</label>
   </div>
   <div class="trip-sort__item  trip-sort__item--price">
-    <input id="sort-price" class="trip-sort__input  visually-hidden" type="radio" name="trip-sort" value="sort-price" checked>
+    <input id="sort-price" class="trip-sort__input  visually-hidden" type="radio" name="trip-sort" value="sort-price" data-sort-type="${SORT_TYPES.PRICE}">
     <label class="trip-sort__btn" for="sort-price">Price</label>
   </div>
   <div class="trip-sort__item  trip-sort__item--offer">
@@ -30,4 +31,16 @@ export default class SortView extends AbstractView {
   get template() {
     return createSortTemplate();
   }
+
+  setSortHandler = (callback) => {
+    this._callback.sort = callback;
+    this.element.addEventListener('click', this.#sortHandler);
+  };
+
+  #sortHandler = (e) => {
+    if (e.target.type !== 'radio') {
+      return;
+    }
+    this._callback.sort(e.target.dataset.sortType);
+  };
 }
